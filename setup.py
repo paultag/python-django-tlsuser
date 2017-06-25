58b5a7d7d60d3caf1c4468641d20f27467a13ce9
Paul Tagliamomnte <tag@pault.ag>
2017-06-25 10:26:29 -0400

    add in a app

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name="tlsuser",
+    version="0.1",
+    packages=[
+        'tlsuser',
+    ],
+    author="Paul Tagliamonte",
+    author_email="paultag@gmail.com",
+    long_description=long_description,
+    description='n/a',
+    license="Expat",
+    url="http://pault.ag/",
+    platforms=['any']
+)
